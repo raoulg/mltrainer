377bf0f4b7c19c7984bdcbe9657ab7e0f9f73af8
R.Grouls <rgrouls@gmail.com>
2023-07-01 21:39:13 +0200

    pyproject

--- mltrainer/__init__.py
+++ mltrainer/__init__.py
@@ -1,6 +1,6 @@
-from mltrainer.trainer import Trainer
 from mltrainer.settings import TrainerSettings, ReportTypes
+from mltrainer.trainer import Trainer
 
 __all__ = ["Trainer", "TrainerSettings", "ReportTypes"]
 
-__version__ = "0.1.0"
+__version__ = "0.1.5"
